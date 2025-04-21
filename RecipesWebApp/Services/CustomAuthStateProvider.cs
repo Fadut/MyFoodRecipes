e3e7e7681ec91075ec6c47dc2a65e93bfcd0339f
Fadut <58516594+Fadut@users.noreply.github.com>
2025-04-21 22:11:16 +0200

    design and frontend

--- RecipesWebApp/Services/CustomAuthStateProvider.cs
+++ RecipesWebApp/Services/CustomAuthStateProvider.cs
@@ -5,30 +5,49 @@ namespace RecipesWebApp.Services
 {
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
-        private ClaimsPrincipal _anonymous = new(new ClaimsIdentity());
+        //private ClaimsPrincipal _anonymous = new(new ClaimsIdentity());
+
+        //public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        //{
+        //    return Task.FromResult(new AuthenticationState(_anonymous));
+        //}
+
+        //public void MarkUserAsAuthenticated(string username)
+        //{
+        //    var identity = new ClaimsIdentity(new[]
+        //    {
+        //    new Claim(ClaimTypes.Name, username),
+        //}, "apiauth_type");
+
+        //    var user = new ClaimsPrincipal(identity);
+
+        //    NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        //}
+
+        private ClaimsPrincipal _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
 
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            return Task.FromResult(new AuthenticationState(_anonymous));
+            return Task.FromResult(new AuthenticationState(_currentUser));
         }
 
         public void MarkUserAsAuthenticated(string username)
         {
             var identity = new ClaimsIdentity(new[]
             {
-            new Claim(ClaimTypes.Name, username),
-        }, "apiauth_type");
-
-            var user = new ClaimsPrincipal(identity);
-
-            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+                new Claim(ClaimTypes.Name, username),
+            }, "apiauth_type");
+          
+            _currentUser = new ClaimsPrincipal(identity);
+           
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_currentUser)));
         }
 
         // Needed? Or remove.
-        public void MarkUserAsLoggedOut()
-        {
-            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_anonymous)));
-        }
+        //public void MarkUserAsLoggedOut()
+        //{
+        //    NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_anonymous)));
+        //}
     }
 
 }
